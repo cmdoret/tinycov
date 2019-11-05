637c5244d654ccacd88dac689d867e73213410cf
cmdoret <cyril.mattheydoret@gmail.com>
2019-11-05 15:46:35 +0100

    add metadata files and test data

---
+++ tinycov/__init__.py
@@ -0,0 +1,17 @@
+"""
+A tiny standalone to quickly visualise coverage from BAM files.
+
+"""
+
+from .tinycov import *
+
+__author__ = "Cyril Matthey-Doret"
+__copyright__ = "Copyright © 2019, Cyril Matthey-Doret"
+__credits__ = [
+    "Cyril Matthey-Doret",
+    "Matt Edwards",
+]
+__license__ = "MIT"
+__maintainer__ = "Cyril Matthey-Doret"
+__email__ = "cyril.matthey-doret@pasteur.fr" 
+__status__ = "Alpha"
